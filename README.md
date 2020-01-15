bc50ff224b5519f126d112ed1a4753546f1265ba
decanus <dean.eigenmann@icloud.com>
2020-01-15 23:45:31 +0100

    updated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # go-libp2p-dns-discovery
-DNS based discovery for libp2p based on EIP-1459
+
+DNS based discovery for libp2p based on [EIP-1459](https://eips.ethereum.org/EIPS/eip-1459), the implementation is heavily inspired by the code found in [go-ethereum](https://github.com/ethereum/go-ethereum/tree/master/p2p/dnsdisc). 
--- discovery.go
+++ discovery.go
@@ -2,13 +2,22 @@ package dns
 
 import (
 	"context"
+	"errors"
+	"strings"
 
 	"github.com/libp2p/go-libp2p-core/discovery"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
-type dnsDiscovery struct {
+const RootPrefix = ""
 
+type Resolver interface {
+	LookupTXT(ctx context.Context, name string) ([]string, error)
+}
+
+type dnsDiscovery struct {
+	domain string
+	r Resolver
 }
 
 func NewDNSDiscovery() discovery.Discoverer {
@@ -23,5 +32,21 @@ func (d *dnsDiscovery) FindPeers(ctx context.Context, ns string, opts ...discove
 		return nil, err
 	}
 
+
 	return nil, nil
+}
+
+func (d *dnsDiscovery) resolveRoot(ctx context.Context) (string, error) {
+	txts, err := d.r.LookupTXT(ctx, d.domain)
+	if err != nil {
+		return "", err
+	}
+
+	for _, txt := range txts {
+		if strings.HasPrefix(txt, RootPrefix) {
+
+		}
+	}
+
+	return "", errors.New("new root record found")
 }
